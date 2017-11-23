abd921381ed514ef2453b66b110c27c5d90c19ba
Matthew Kotys <mattkotys@gmail.com>
2017-11-23 13:38:46 -0600

    Through Styling in React https://www.kirupa.com/react/styling_in_react.htm

---
+++ src/components/Buttonify.js
@@ -0,0 +1,11 @@
+class Buttonify extends React.Component {
+    render() {
+        return(
+            <div>
+                <button type={this.props.behavior}>{this.props.children}</button>
+            </div>
+        )
+    }
+}
+
+export default Buttonify
